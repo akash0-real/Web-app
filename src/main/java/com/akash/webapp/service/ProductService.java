521bc8cafea44b3c4b4802b80e4ee78ff7fca62e
akash <aayush45r@gmail.com>
2025-10-31 19:21:42 +0530

    added a client side product add method

--- src/main/java/com/akash/webapp/service/ProductService.java
+++ src/main/java/com/akash/webapp/service/ProductService.java
@@ -2,19 +2,27 @@ package com.akash.webapp.service;
 
 import com.akash.webapp.Model.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> product = Arrays.asList(new Product(101,"apple",50000), new Product(102,"mingos",40000));
+    List<Product> product = new ArrayList<>(
+            Arrays.asList(new Product(101,"apple",50000),
+                    new Product(102,"mingos",40000)));
     public List<Product> getProducts(){
         return product;
     }
     public Product getProductId(int prodId){
         return  product.stream().filter(n -> n.getProdId() == prodId)
                 .findFirst().orElse(new Product(1,"No Item",0));
+    }
+
+    public void addProd(Product prod){
+        product.add(prod);
     }
 }
