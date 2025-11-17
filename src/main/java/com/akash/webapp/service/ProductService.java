78f4ae1cf3e0c5a4e506dc094b70ebdf3fb5561e
akash <aayush45r@gmail.com>
2025-11-17 13:26:56 +0530

    refactored the code with repo class

--- src/main/java/com/akash/webapp/service/ProductService.java
+++ src/main/java/com/akash/webapp/service/ProductService.java
@@ -1,47 +1,38 @@
 package com.akash.webapp.service;
 
 import com.akash.webapp.Model.Product;
+import com.akash.webapp.repo.ProdRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class ProductService {
 
-    List<Product> product = new ArrayList<>(
-            Arrays.asList(new Product(101,"apple",50000),
-                    new Product(102,"mingos",40000)));
+    private final ProdRepo repo;
+    @Autowired
+    ProductService(ProdRepo repo){
+        this.repo = repo;
+    }
+
+
     public List<Product> getProducts(){
-        return product;
+        return repo.findAll() ;
     }
     public Product getProductId(int prodId){
-        return  product.stream().filter(n -> n.getProdId() == prodId)
-                .findFirst().orElse(new Product(1,"No Item",0));
+        return  repo.findById(prodId).orElse(new Product());
     }
 
     public void addProd(Product prod){
-        product.add(prod);
+        repo.save(prod);
     }
 
     public void updateProd(Product prod){
-        int index = 0;
-        for(int i=0;i<product.size();i++){
-            if(product.get(i).getProdId() == prod.getProdId()){
-                index = i;
-            }
-        }
-        product.set(index,prod);
+        repo.save(prod);
     }
 
     public void deleteProd(int prodId){
-        int index = 0;
-        for(int i=0;i<product.size();i++){
-            if(product.get(i).getProdId() == prodId){
-                index = i;
-            }
-        }
-        product.remove(index);
+        repo.deleteById(prodId);
 
     }
 }
